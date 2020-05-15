a32d060a2ef0f92b81d8815182d3b0ef84a4f0ef
Amanda Siqueira Rosa <amandasrosa@hotmail.com>
2020-05-15 14:59:21 -0300

    Talk about Swift Lint

---
+++ SwiftLint/SwiftLint.playground/Pages/custom-configs.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+/*:
+ # 3. Configurações customizadas
+ */
+/*:
+
+ # Abra o .swiftlint.yml
+ - open .swiftlint.yml
+
+ ![swiftlintarquivo](swiftlintarquivo.png)
+ 
+ **Não adicione qualquer coisa em included / excluded**
+
+ 
+ # Crie sua própria regra
+ ![swiftlintarquivo](swiftlintarquivo2.png)
+
+ 
+ 
+ # Desabilite regras dentro do código
+ ![swiftlintarquivo](swiftlintdisable.png)
+ 
+ */
+
+//: [Anterior](@previous) | [Próxima](@next)
---
+++ SwiftLint/SwiftLint.playground/Pages/installing.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+/*:
+ # 2. Instalando
+ */
+/*:
+ ## Usando HomeBrew
+ -> brew install swiftlint
+ 
+ 
+ ## Usando CocoaPods
+ -> pod 'SwiftLint'
+ 
+ 
+ ## Cria arquivo swiftlint na raiz do projeto
+ touch .swiftlint.yml
+
+ 
+ ## Cria script dentro de Build Phases
+ 1. Dentro do Xcode clica no projeto
+ 2. Clica no primeiro Target
+ 3. Dentro das configurações clica em Build Phases
+ 4. Clica no + New Run Script Phase (pode renomeá-lo depois)
+ 5. Cola o comando abaixo:
+
+ ![install](lintInstall.png)
+ 
+ 
+ Builda o projeto e alguns warnings já devem aparecer.
+*/
+
+/*:
+****
+[Anterior](@previous) | [Próxima](@next)
+*/
