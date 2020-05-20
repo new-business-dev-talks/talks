bbdc956927d73fc0f6606817d7c27d9f707a261c
Roberth Siqueira <roberth.siqueira@gmail.com>
2020-05-20 20:05:26 -0300

    Talk about Buck

---
+++ Buck/Buck.playground/Pages/Interação.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+/*:
+ ## Se não usamos o Xcode, como interagirmos com o Buck?
+ ### por Terminal 🤓
+*/
+/*:
+ ****
+ [Previous](@previous) | [Next](@next)
+ */
---
+++ Buck/Buck.playground/Pages/Quem usa.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+/*:
+ ## Quem usa 🙋🏽‍♂️
+ */
+/*:
+* [Buck](https://buck.build/about/showcase.html)
+* [Bazel](https://bazel.build/)
+*/
+/*:
+ ****
+ [Previous](@previous) | [Next](@next)
+ */
